256f3b449467b01731ea53b8f6c83bacd17c874a
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-09-27 16:14:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// 01
+var integer = 102
+console.log(integer)
+var float = 13.9
+console.log(float)
+
+//02
+var basic = 34
+console.log(basic)
+console.log(basic.lenght)
+basic = basic.toString();
+console.log(basic)
+
+//03
+var num = 1.5
+var rounded = 1.5
+
+console.log(Math.round(num))
+
